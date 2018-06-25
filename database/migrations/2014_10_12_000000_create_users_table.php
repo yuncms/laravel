f933aa82049aa4e20aeb11dc032b8c8196c05ad2
Xu Tongle <xutongle@gmail.com>
2018-06-25 12:17:43 +0800

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name', 50)->index();//用户昵称。
-            $table->string('email', 64)->unique();
-            $table->tinyInteger('identified')->default(0);
-            $table->tinyInteger('disabled')->default(0);
+            $table->string('email', 64)->unique();//邮箱
+            $table->string('mobile')->index();//手机号码
+            $table->string('avatar', 255);//头像地址
+            $table->string('gender', 10);//性别。MALE：男，FEMALE：女。
+            $table->boolean('identified')->default(false);//是否经过实名认证。
+            $table->boolean('disabled')->default(false);//是否被禁用
             $table->decimal('withdrawable_balance')->default(0.00);//可提现余额，可用于消费、提现、转账等。
             $table->decimal('available_balance')->default(0.00);//可用余额，可用于消费。
             $table->string('password', 100);
